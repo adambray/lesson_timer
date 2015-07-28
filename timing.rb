7b1f36900f46e9dba3c756c06f8c46bcfc93203d
mattscilipoti <matt.scilipoti@ga.co>
2015-07-28 11:54:39 -0400

    Add optional start time

--- timing.rb
+++ timing.rb
@@ -1,22 +1,22 @@
-FILE_PATH = ARGV[0]
+require 'time' # for Time.parse
 
-def convert_to_hours_and_minutes(num_minutes)
-  minutes = num_minutes % 60
-  hours = (num_minutes / 60)
-  format("%02d:%02d", hours, minutes)
-end
+FILE_PATH = ARGV[0]
 
 new_file = []
 summary = []
-total_time = 0
+
+# always uses start_time, if none passed start_time is midnight.
+start_time = ARGV[1] ? Time.parse(ARGV[1]) : Time.parse('00:00')
+elapsed_time = start_time
 
 File.open(FILE_PATH, 'r') do |file|
   file.each do |line|
     match = line.match /(^\#+.*\()(\d+)(.*)(\))/
 
     if match
-      total_time += match[2].to_i
-      updated_header = match[1] + match[2] + match[3] + " - #{convert_to_hours_and_minutes(total_time)}" + match[4]
+      portion_minutes = match[2].to_i
+      elapsed_time += (portion_minutes * 60)
+      updated_header = match[1] + match[2] + match[3] + " - #{elapsed_time.strftime "%H:%M"}" + match[4]
 
       new_file << updated_header
       summary << updated_header
@@ -26,11 +26,12 @@ File.open(FILE_PATH, 'r') do |file|
 
   end
 end
+file_basename = File.basename(FILE_PATH, '.*')
 
-File.open("#{FILE_PATH[0...-3]}_timed.md", "w") do |file|
+File.open("#{file_basename}_timed.md", "w") do |file|
   file.puts new_file
 end
 
-File.open("#{FILE_PATH[0...-3]}_summary.md", "w") do |file|
+File.open("#{file_basename}_summary.md", "w") do |file|
   file.puts summary
 end

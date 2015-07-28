c8152dcbc2bcbad9bb491cc62c5388499a9cd602
mattscilipoti <matt.scilipoti@ga.co>
2015-07-28 12:13:27 -0400

    simplified to File.readlines().each

--- timing.rb
+++ timing.rb
@@ -9,23 +9,20 @@ summary = []
 start_time = ARGV[1] ? Time.parse(ARGV[1]) : Time.parse('00:00')
 elapsed_time = start_time
 
-File.open(FILE_PATH, 'r') do |file|
-  file.each do |line|
-    match = line.match /(^\#+.*\()(\d+)(.*)(\))/
+File.readlines(FILE_PATH).each do |line|
+  match = line.match /(^\#+.*\()(\d+)(.*)(\))/
 
-    if match
-      portion_minutes = match[2].to_i
-      portion_start = elapsed_time
-      elapsed_time += (portion_minutes * 60)
-      portion_end = elapsed_time
-      updated_header = match[1] + match[2] + match[3] + "; #{portion_start.strftime("%H:%M")}-#{portion_end.strftime("%H:%M")}" + match[4]
+  if match
+    portion_minutes = match[2].to_i
+    portion_start = elapsed_time
+    elapsed_time += (portion_minutes * 60)
+    portion_end = elapsed_time
+    updated_header = match[1] + match[2] + match[3] + "; #{portion_start.strftime("%H:%M")}-#{portion_end.strftime("%H:%M")}" + match[4]
 
-      new_file << updated_header
-      summary << updated_header
-    else
-      new_file << line
-    end
-
+    new_file << updated_header
+    summary << updated_header
+  else
+    new_file << line
   end
 end
 file_basename = File.basename(FILE_PATH, '.*')
